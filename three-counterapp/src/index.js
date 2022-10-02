304a7efc5945d285f32afbd6272981c259986fff
Apple <apple@Apples-MacBook-Air.local>
2022-10-02 18:34:53 +0400

    react counterapp

---
+++ three-counterapp/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App';
+
+
+// render(<App />, document.getElementById('root'))
+createRoot(document.getElementById('root')).render(<App />)
