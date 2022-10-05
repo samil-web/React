2a2f3a657b341b3b15a6e656d3e64e5aefdf6ebb
Apple <apple@Apples-MacBook-Air.local>
2022-10-05 10:34:43 +0400

    tictactoe

---
+++ four-tictactoe/src/Grid.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Grid.css';
+const Grid = () => {
+    return (
+        <div className="box1">
+            <i className="fa-brands fa-react"></i>
+        </div>
+
+    )
+}
+export default Grid;
